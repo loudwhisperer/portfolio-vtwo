063024e8300c2f6855fc588376033cca9f963066
loudwhisperer <driftaway44@gmail.com>
2023-05-11 18:42:43 -0600

    added daisy ui footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,32 +30,49 @@ export default function Footer(){
     },
   ];
     return (
-    <footer className="bg-base-100 m-1 shadow-md w-full fixed bottom-0 left-0">
-    <div className="max-w-screen-xl px-2 py-6 mx-auto space-y-6 overflow-hidden sm:px-6 lg:px-8">
-        <nav className="flex flex-wrap justify-center -mx-5 -my-2 z-[-1]">
-                   <ul
-              className={"flex md:items-center md:pb-0 pb-12 md:static md:z-auto sm:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 sm:flex sm:items-center sm:pb-0 sm:static"
-              }
-            >
-           {Something.map((totem) => {
+//     <footer className="bg-base-100 m-1 shadow-md w-full fixed bottom-0 left-0">
+//     <div className="max-w-screen-xl px-2 py-6 mx-auto space-y-6 overflow-hidden sm:px-6 lg:px-8">
+//         <nav className="flex flex-wrap justify-center -mx-5 -my-2 z-[-1]">
+//                    <ul
+//               className={"flex md:items-center md:pb-0 pb-12 md:static md:z-auto sm:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 sm:flex sm:items-center sm:pb-0 sm:static"
+//               }
+//             >
+          //  {Something.map((totem) => {
+          //       return (
+          //         <li key={totem.id} className="text-4xl lg:mr-10  sm:mr-2 px-5 py-2 sm:text-2xl"> 
+          //         <Link
+          //             to={totem.link}
+          //             className="text-4xl text-white mr-3 pt-2"
+          //             target="_blank"
+          //           >
+          //             <ion-icon name={totem.icon}></ion-icon>
+          //           </Link>
+          //         </li>
+          //       );
+          //     })}
+//               </ul>
+//         </nav>
+//         <p className="mt-8 text-base leading-6 text-center text-white">
+//             © 2023 Brendan Borowski
+//         </p>
+//     </div>
+// </footer>
+<footer className="footer  p-4 text-neutral-content w-full fixed bottom-0 left-0 bg-base-100">
+  <div className="grid-flow-col lg:gap-28 place-self-center justify-self-center">
+                {Something.map((totem) => {
                 return (
-                  <li key={totem.id} className="text-4xl lg:mr-10  sm:mr-2 px-5 py-2 sm:text-2xl"> 
+                  
                   <Link
                       to={totem.link}
-                      className="text-4xl text-white mr-3 pt-2"
+                      className="text-4xl text-white mr-3 pt-2 fill-current"
                       target="_blank"
+                      key={totem.id}
                     >
                       <ion-icon name={totem.icon}></ion-icon>
                     </Link>
-                  </li>
                 );
               })}
-              </ul>
-        </nav>
-        <p className="mt-8 text-base leading-6 text-center text-white">
-            © 2023 Brendan Borowski
-        </p>
-    </div>
+  </div>
 </footer>
     );
 }
